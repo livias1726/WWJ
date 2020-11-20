97f67ff41a86e900b5205866fc17701586517669
Livia Simoncini <69426507+livias1726@users.noreply.github.com>
2020-11-20 20:41:44 +0100

    Issue #19 - 2nd sonarcloud fix

--- src/logic/util/ImageBucket.java
+++ src/logic/util/ImageBucket.java
@@ -8,7 +8,11 @@ public class ImageBucket {
 	
 	private static final String PATH = "src\\graphics\\";	
 	public static final String MAIN = "main_icon";
-		
+	
+	private ImageBucket() {
+		/*default constructor*/
+	}
+	
 	public static Image getImage(String name) {
 		File file = new File(PATH + name + ".png");
 		return new Image(file.toURI().toString());
