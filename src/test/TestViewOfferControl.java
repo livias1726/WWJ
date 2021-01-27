2d7412db57b309fb9ce049132b330eebdcc2cc9b
Livia Simoncini <69426507+livias1726@users.noreply.github.com>
2021-01-27 10:34:30 +0100

    Testing issue #35

---
+++ src/test/TestViewOfferControl.java
@@ -0,0 +1,37 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import logic.application.control.ViewOfferControl;
+import logic.exceptions.DatabaseFailureException;
+import logic.exceptions.NoResultFoundException;
+import logic.presentation.bean.CountryBean;
+import logic.presentation.bean.JobBean;
+
+public class TestViewOfferControl {
+
+	@Test
+	public void testRetrieveOffersByJobNoResult() {
+		JobBean noJob = new JobBean();
+		noJob.setId(0);
+		
+		assertThrows(NoResultFoundException.class, () -> ViewOfferControl.getInstance().retrieveOffersByJob(noJob));
+	}
+	
+	@Test
+	public void testRetrieveOffersByCountryNoResult() throws DatabaseFailureException {
+		CountryBean noCountry = new CountryBean();
+		noCountry.setName("NoCountry");
+		
+		try {
+			ViewOfferControl.getInstance().retrieveOffersByCountry(noCountry);
+			fail("Exception not thrown");
+		}catch(NoResultFoundException e) {
+			assertEquals("No results found.", e.getMessage());
+		}
+		
+	}
+
+}
